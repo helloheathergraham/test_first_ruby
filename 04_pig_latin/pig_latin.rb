da352b2a55c87d8cee7f72be28db6c1d479d8f60
Heather <helloheathergraham@gmail.com>
2014-01-20 18:20:20 -0500

    pig latin

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,16 @@
+def translate(word)
+	alpha = ('a'..'z').to_a
+  	vowels = %w[a e i o u]
+  	consonants = alpha - vowels
+
+	if vowels.include?(word[0])
+		word + "ay"
+	elsif consonants.include?(word[0]) && consonants.include?(word[1])
+		word[2..-1] + word[0..1] + 'ay'
+	elsif consonants.include?(word[0])
+		word[1..-1] + word[0] + 'ay'	
+	else 
+		word
+	end
+	
+end
